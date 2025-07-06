6e7ca6319551d7c9779b13a6d1f1dbbc15a54ab7
emanuelcastillo <86929598+emanuelcastillo@users.noreply.github.com>
2025-07-05 19:43:01 -0600

    update

--- backend/src/main/java/gt/helpdesk/backend/config/security/SecurityConfig.java
+++ backend/src/main/java/gt/helpdesk/backend/config/security/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
         );
         http.cors(cors -> cors.configurationSource(request -> {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(List.of("https://*.overhare.com", "http://localhost:4200", "https://*.helpdesk.gt"));
+            configuration.setAllowedOrigins(List.of("https://*.overhare.com", "http://localhost:4200", "https://self.helpdesk.gt"));
             configuration.setAllowedMethods(List.of(
                     "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"
             ));
